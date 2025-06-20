f867ccf7f9820c92ce4ec54df1818c48a3644861
Faiq Shamass <faiq.shamass@gmail.com>
2025-06-20 15:33:05 -0700

    Time: 0 ms (100.00%) | Memory: 63 MB (12.45%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,9 +4,14 @@ public:
         int left = 0;
         int right = height.size()-1;
         int maxWater = 0;
+        //Strategy is to to have 2 pointers left and right
+        //Find the water between them and then move the shortest height
         while(left < right) {
+            //Find water stored between current container formed by left and right pointers
             int water = (right - left) * min(height[left],height[right]);
+            //Is it max water seen so far
             maxWater = max(maxWater, water);
+            //Move the shortest height
             if(height[left] < height[right]) left++;
             else right--;
         }
